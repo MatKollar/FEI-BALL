8d33972620d8f9377a083409d007966079dbaecc
MatKollar <matej.kollar3@gmail.com>
2022-12-29 21:51:36 +0100

    Edit PWA

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,8 @@
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open("my-cache").then(function (cache) {
-      return cache.addAll([
-        "/index.html",
-        "/game.html",
-        "/howtoplay.html",
-        "/description.html",
-        "/css/game.css",
-        "/css/description.css",
-        "/css/howtoplay.css",
-        "/css/index.css",
-        "/css/print.css",
-        "/js/game.js",
-        "/js/assets/Ball.js",
-        "/js/assets/Board.js",
-        "/js/assets/GameLogic.js",
-        "/js/assets/Stage.js",
-        "/js/assets/Timer.js",
-        "/images/FEI_BALL_BG.png",
-        "/images/FEI-BALL.png",
-        "/images/feiball_favicon.png",
-        "/images/gameover.png",
-        "/images/Instructions_mobile.png",
-        "/images/Instructions.png",
-        "/images/life.png",
-        "/images/Victory.png",
-      ]);
-    })
-  );
-});
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
+);
+
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.NetworkFirst()
+);
